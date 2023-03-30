9386ca5903c761dccbf1b7668b8f320df671a851
Pietromuresu <muresupietro66@gmail.com>
2023-03-30 21:58:43 +0200

    if you elude all bombs - win . Exercise ended

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,10 +5,11 @@ const generate = document.querySelector('.button-genera');
 const levels = document.getElementById('levels');
 let levelValue = levels.value;
 const bombs = [];
-const NUM_BOMBS =16;
+const NUM_BOMBS =3;
 let points = 0;
 const endGame = document.querySelector('.endGame');
-
+const arrSquare = [];
+console.log(arrSquare);
  
 generate.addEventListener('click', function(){
   generate.classList.add('hide');
@@ -18,23 +19,34 @@ generate.addEventListener('click', function(){
   for (let i = 1; i <= levels.value; i++){
   const newDiv = document.createElement("div");
   grid.appendChild(newDiv);
+  arrSquare.push("newDiv.idElem ");
   newDiv.classList.add('square');
   newDiv.idElem = i ;
   
 
   newDiv.addEventListener('click', function(){
   newDiv.classList.add('clicked');
-    
+  
+  
   
     if (bombs.includes(newDiv.idElem)){
       console.log('hai perso');
+      newDiv.classList.add('bomb');
       endGame.classList.add('showEnd');
-      endGame.innerHTML = `<h1> HAI PERSO! HAI TOTALIZZATO ${points} PUNTI </h1>  `
+      endGame.innerHTML = `<h1 class="lose"> HAI PERSO! HAI TOTALIZZATO ${points} PUNTI </h1>  `
     }else{
       points++;
     }
+    if (points === (arrSquare.length - bombs.length)){
+          console.log('hai vinto');
+          endGame.classList.add('showEnd');
+          endGame.innerHTML = `<h1 class="win" > HAI VINTO! HAI SCHIVATO TUTTE LE BOMBE!!! </h1>  `
+
+        }
     })
-    console.log(points);
+    
+    
+  
 
   if (levels.value === '100'){
   newDiv.classList.add('levelOne');
