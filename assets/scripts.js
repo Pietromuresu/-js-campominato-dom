cd6ccb6f49eab6dd6150bb1b58ead4eddb3bd5b4
Pietromuresu <muresupietro66@gmail.com>
2023-03-30 17:19:00 +0200

    add files from  "js-campominato-grid" repository

---
+++ assets/scripts.js
@@ -0,0 +1,36 @@
+
+const grid = document.getElementById('grid');
+const gridContainer = document.querySelector('.container-grid ')
+const generate = document.querySelector('.button-genera');
+const levels = document.getElementById('levels');
+
+
+generate.addEventListener('click', function(){
+  generate.classList.add('hide');
+  gridContainer.classList.remove('hide');
+  levels.classList.add('hide');
+  
+  for (let i = 1; i <= levels.value; i++){
+  const newDiv = document.createElement("div");
+  grid.appendChild(newDiv);
+  newDiv.classList.add('square');
+  newDiv.idElem = i ;
+  newDiv.addEventListener('click', function(){
+    newDiv.classList.add('clicked');
+    
+    console.log(newDiv.idElem);
+    })
+
+  if (levels.value === '100'){
+  newDiv.classList.add('levelOne');
+  }else if (levels.value === '81'){
+  newDiv.classList.add('levelTwo');
+  }else {
+  newDiv.classList.add('levelThree');
+  }
+  }
+  
+})
+
+
+
